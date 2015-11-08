5d1ce2ab2610a724f705922f84f47e2fae513419
ccisko <ccisko317@gmail.com>
2015-11-08 00:21:43 -0500

    final

--- js2/script.js
+++ js2/script.js
@@ -1,30 +1,34 @@
+//Default Map settings
 var map = new google.maps.Map(document.getElementById('map-canvas'),{zoom: 15,center: new google.maps.LatLng(41.4562186,-72.8224525) });
+// creating locations
 var makingPlaces = function(placeData) {
   this.name = ko.observable(placeData.name);
   this.description = ko.observable(placeData.description);
   this.address = ko.observable(placeData.address);
   this.lat = ko.observable(placeData.lat);
   this.lng = ko.observable(placeData.lng);
-  this.myfunamount = ko.observableArray(['p']);
+  this.myfunamount = ko.observableArray();
+  this.myfunitem = ko.observableArray();
   this.marker = new google.maps.Marker({
     position: new google.maps.LatLng(placeData.lat,placeData.lng),
     animation: google.maps.Animation.DROP,
     title: this.name(),
     map: map
   });
+  //reset markers
   this.togglemkr = function(mymaponoff) {
     this.marker.setMap(mymaponoff);
-  }
-  this.infowindowContent = ko.computed(function() {
-    return "<h2>" + this.name() + "</h2> " + "<h3>" + this.description() + "</h3> "  + "<h4>" + this.address() + "</h4> " + "<h5>" + "USDA Fun Facts:" + "</h5>" +  "<h5>"  +"<br>" +  this.myfunamount() + " calories" + "</h5>"},this )
-this.infowin = new google.maps.InfoWindow({content: this.infowindowContent()  });
-}
+  };
+};
+// start viewmodel
 var viewModel = function() {
   var self = this;
+  // input box
   self.searchPlaces = ko.observable('');
+  // create array of locations
   self.locations = ko.observableArray([]);
   myPlaces.forEach(function(placeItem) {
-    self.locations.push(new makingPlaces(placeItem));
+  self.locations.push(new makingPlaces(placeItem));
   });
   self.searchedLocations = ko.computed(function() {
     var myplacesarray =[];
@@ -39,65 +43,74 @@ var viewModel = function() {
         self.locations()[i].togglemkr();
       }
     }
-  return myplacesarray },this)
-  this.currentPlace = ko.observable(this.searchedLocations()[1]);
-  this.selcurrentPlace = function(clickedPlace) {
-    self.currentPlace(clickedPlace);
-    clickedPlace.marker.setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout(function() {
-      clickedPlace.marker.setAnimation(null);
-    },800);
-    clickedPlace.infowin.open(map, clickedPlace.marker);
-  }
+  return myplacesarray; },this);
+
   this.searchedLocations().forEach(function(placeItem) {
     google.maps.event.addListener(placeItem.marker, 'click', function() {
       self.showpopup(placeItem);
     });
   });
-  self.showpopup = function(placeItem){//1
+  // infowindows and second API
+  self.showpopup = function(placeItem){
+    if (self.infowinmarker) {
+      self.infowinmarker.close(map, placeItem.marker);
+    }
     var nutrition_item  = placeItem.description();
-    var myfunitem;
-	  console.log(nutrition_item);
-    myUrl = "https://api.nutritionix.com/v1_1/search/" + nutrition_item + "?fields=item_name,brand_name,nf_calories&appId=074b01eb&appKey=9f1a554a75a4427b22051e94ad1724cd"
-    $.ajax({//2
+    myUrl = "https://api.nutritionix.com/v1_1/search/" + nutrition_item + "?fields=item_name,brand_name,nf_calories&appId=074b01eb&appKey=9f1a554a75a4427b22051e94ad1724cd";
+
+    $.ajax({
       type: "GET",
       dataType: "json",
-      limit: 10,
+      limit: 20,
       url:  myUrl,
-      success: function(response) {//3
-        stp = 0;
-        while (stp == 0) {//4
+	  error: function(response) {
+        console.log("i have an error");
+        placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(
+            function() {
+              placeItem.marker.setAnimation(null);
+            },800);
+            self.infowinmarker = new google.maps.InfoWindow({
+              maxHeight: 100,
+              maxWidth: 140
+            });
+        placeItem.infowindowContent = ko.computed(function() {
+          return "<h2>" + placeItem.name() + "</h2> " + "<h5>" +"Menu Items:" +  placeItem.description() + "</h5>"  + "<h4>" + placeItem.address() + "</h4> " + "<h5>" + "<h2>" + "Sorry having trouble getting Calorie information , perhaps you reached daily limit of 500 queries." + "</h2> ";},this );
+        self.infowinmarker.setContent(placeItem.infowindowContent());
+        self.infowinmarker.open(map, placeItem.marker);
+      },
+      success: function(response) {
+        while (true) {
           i= Math.floor((Math.random() * 9) + 1);
-          if (response.hits[i].fields != undefined ) {//5
-            if (response.hits[i].fields.brand_name = "USDA") {//6
-//	console.log(response.hits[i]);
-//     console.log(response.hits[i].fields.item_name);
-// console.log(response.hits[i].fields.brand_name);
-              myfunitem  = (response.hits[i].fields.item_name);
-              myfuncalorie = (response.hits[i].fields.nf_calories) ;
-              placeItem.myfunamount().push('lmn');
-//  placeItem.myfunname.push(placeItem.description);
-//	placeItem.myfunamount.push(response.hits[i].fields.nf_calories);
-              console.log(response.hits[i].fields.nf_calories) 
-//      console.log(response.total_hits);
-              stp =1 ;
-            }//6
-          }//5
-        }//4
-      }//3
-    })//2
-//Bounce markers with a time out of 800
-    placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout(
-    function() {//2
-      placeItem.marker.setAnimation(null);
-    },800);//2
-    self.infowinmarker = new google.maps.InfoWindow({
-      maxHeight: 150,
-      maxWidth: 200
+          if (response.hits[i].fields != undefined ) {
+		  if (response.hits[i].fields.brand_name = "USDA") {
+            var myfunamounttest = response.hits[i].fields.nf_calories;
+            var myfunitemtest = response.hits[i].fields.item_name;
+          }
+          placeItem.myfunamount.removeAll();
+          placeItem.myfunitem.removeAll();
+          placeItem.myfunamount().push(myfunamounttest);
+          placeItem.myfunitem().push(myfunitemtest);
+          placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+		  //Bounce markers with a time out of 800
+            setTimeout(
+              function() {
+                placeItem.marker.setAnimation(null);
+              },800);
+              self.infowinmarker = new google.maps.InfoWindow({
+              maxHeight: 100,
+              maxWidth: 140
+            });
+          placeItem.infowindowContent = ko.computed(function() {
+            return "<h2>" + placeItem.name() + "</h2> " + "<h5>" +"Menu Items" + "</h5>" + "<br>" +"<h3>" + placeItem.description() + "</h3> "  + "<h4>" + placeItem.address() + "</h4> " + "<h5>" + "USDA Fun Facts:" + "<br>" + placeItem.myfunitem()[0] + "<br>" +  placeItem.myfunamount()[0] + " calories" + "</h5>";},this );
+            self.infowinmarker.setContent(placeItem.infowindowContent());
+            self.infowinmarker.open(map, placeItem.marker);
+          break;
+          }
+        }
+      }
     });
-    self.infowinmarker.setContent(placeItem.infowindowContent());
-    self.infowinmarker.open(map, placeItem.marker);
-  };//1
-}
+
+  };
+};
 ko.applyBindings(new viewModel());
--- readme.txt
+++ readme.txt
@@ -1,15 +1,16 @@
 readme file .
 
 this is project (5) Neighborhood map
-open index.html 
+1.open index.html 
 A list of places to Eat in Wallingford Ct will appear.
-use the search box to narrow the amout of choices.
-You ca type in the Nam of the Business or type in some of the menu 
+use the search box to narrow the amount of choices.
+You can type in the Name of the Business or type in some of the menu 
 items that are available to you. 
 for example if you type in Mr or cupcakes it will display Mr D's 
 
-from the list  you can click o a marker or on the business name and 
-an information widow will open giving you the street address and some menu choices.
-It will also give a random menu Item with the USDA calories for that item .
+from the list  you can click on a marker or on the business name and 
+an information window will open giving you the street address and a menu choice.
+It will also give a random Item with the USDA calories for that item .
+for example if you are on Stella's pizza you might get calories for cheese pizza or pepperoni pizza.
 
 
